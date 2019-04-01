081446903139158678e1ba9a929055761c3997b6
SailShaw <ShawSail@live.cn>
2019-04-01 22:38:00 +0800

    changed:登录

--- src/main/java/com/springboot/demo/controller/LoginController.java
+++ src/main/java/com/springboot/demo/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.controller;
 
+import cn.hutool.core.util.RandomUtil;
 import com.springboot.demo.core.interceptor.aop.Operation;
 import com.springboot.demo.entity.User;
 import com.springboot.demo.mapper.UserMapper;
@@ -7,6 +8,7 @@ import com.springboot.demo.service.IUserService;
 import com.springboot.demo.util.MD5;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,8 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Create By SINYA
@@ -26,7 +30,6 @@ public class LoginController {
     private final static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 
-
     @Resource
     private IUserService userService;
 
@@ -36,48 +39,133 @@ public class LoginController {
     @Operation(value = "注册账号")
     @RequestMapping("register")
     public String register(User user){
+        String result = null;
 
         //小写转换
         user.setAccount(user.getAccount().toLowerCase());
         user.setEmail(user.getEmail().toLowerCase());
-        user.setPasscode(user.getPasscode().toLowerCase());
+        user.setPassword(user.getPassword().toLowerCase());
 
         //已注册查询
-        User censor = userMapper.findUserByAccount(user);
+        User censor = userMapper.findUserByUser(user);
         if (censor != null) {
-            return "USER_IS_EXIST";
+            logger.error("register() -> USER_IS_EXIST");
+            result =  "USER_IS_EXIST";
         }else {
             userService.register(user);
-            return "SUCCESS";
+            result =  "SUCCESS";
         }
-
+        return result;
     }
 
     @Operation(value = "登录系统")
     @RequestMapping("login")
-    public String login(HttpServletRequest request,
-                        User user) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+    public String login(HttpServletRequest request, User user)
+            throws UnsupportedEncodingException, NoSuchAlgorithmException {
+
+        String result = null;
         //账号是否存在
-        User censor = userMapper.findUserByAccount(user);
+        User censor = userMapper.findUserByUser(user);
         if (censor == null) {
             logger.error("用户名不存在");
             return "USER_NOT_EXIST";
         }else {
-            boolean result= MD5.checkpassword(user.getPasscode(),censor.getPasscode());
-            if (result == true) {
+            boolean flag= MD5.checkpassword(user.getPassword(),censor.getPassword());
+            if (flag == true) {
+                //记录登录日期
+                userService.recordLoginTime(censor);
+                //存入session前将密码置为null
+                censor.setPassword(null);
                 //存入session
                 request.getSession().setAttribute("user",censor);
-                return "success";
+                result =  "SUCCESS";
             }else {
-                return "login";
+                logger.error("login() -> PASSWORD_NOT_MATCH");
+                result =  "PASSWORD_NOT_MATCH";
             }
+        }
+        return result;
+    }
+
+    /**
+     * 获取邮箱发送验证码
+     * @Descript:使用session存储user对象和验证码
+     */
+    @RequestMapping("sendResetPasswordLink")
+    public String sendResetPasswordLink(HttpServletRequest request,User user){
+        String result = null;
+        //验证码
+        int verifyCode = RandomUtil.randomInt(100000,999999);
+        Map<String,Object> map = new HashMap<>();
+        //获取邮箱地址
+        String account = request.getParameter("account").toLowerCase();
+        String email = request.getParameter("email").toLowerCase();
+        //注入对象
+        user.setAccount(account);user.setEmail(email);
+        //判空(空指针异常处理)
+        try{
+            //查询账号信息(账号邮箱不匹配时user为空)
+            User userInfo = userMapper.findUserByUser(user);
+            map.put("verifyCode",verifyCode);
+            map.put("userInfo",userInfo);
+            //存入session
+            request.getSession().setAttribute("content",map);
+            //发送
+            userService.sendVerificationCode(verifyCode,userInfo);
+            logger.info("sendResetPasswordLink -> 成功");
+            result =  "SUCCESS";
+        }catch (NullPointerException e){
+            logger.error("findUserByEmail() -> NullPointerException");
+            result =  "ACCOUNT_NOT_MATCH_EMAIL";
+            //移除session
+            request.getSession().removeAttribute("content");
+        }finally {
+            return result;
         }
     }
 
+    /**
+     * 根据验证码重置密码
+     * @param request
+     * @return
+     */
+    @RequestMapping("resetPasswordByCode")
+    public String resetPasswordByCode(HttpServletRequest request){
+        String result = null;
+        //获取session中的数据
+        Map<String,Object> map = new HashMap<>();
+        map = (Map<String, Object>) request.getSession().getAttribute("content");
+
+        String verifyCode = request.getParameter("verifyCode");
+        String password = request.getParameter("passcode");
+        User userInfo = (User) map.get("userInfo");
+        //注入对象(判空处理)
+        if ((!StringUtils.isEmpty(verifyCode)) && verifyCode.equals(map.get("verifyCode") + "")) {
+            userInfo.setPassword(password);
+            userService.resetPassword(userInfo);
+            //移除session
+            request.getSession().removeAttribute("content");
+            result =  "SUCCESS";
+
+        }else{
+            logger.error("resetPasswordByCode():error->"+verifyCode);
+            result =  "VERIFYCODE_NOT_MATCH";
+        }
+        return result;
+    }
+
+    /**
+     * 注销
+     * @param request
+     * @return
+     */
     @RequestMapping("/logout")
     public String logout(HttpServletRequest request){
+        String result = null;
+        //移除session
         request.getSession().removeAttribute("user");
-        return "success";
+        result =  "SUCCESS";
+        return result;
     }
 
 
